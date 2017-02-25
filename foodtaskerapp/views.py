b234f0126e764b5b9b8fcb66fccccf1de3017f69
Kevin <baranyizigiyekevin@gmail.com>
2017-02-25 21:02:18 +0300

    Added lot of new functions/features

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from foodtaskerapp.forms import UserForm, RestaurantForm
+from foodtaskerapp.forms import UserForm, RestaurantForm, UserFormForEdit , MealForm
 from django.contrib.auth import authenticate ,login
+
+
 from django.contrib.auth.models import User
+from foodtaskerapp.models import Meal, Order
 
 # Create your views here.
 def home(request):
@@ -10,7 +13,84 @@ def home(request):
 
 @login_required(login_url = '/restaurant/sign-in/')  #if not logged in redirect to sign-in page
 def restaurant_home(request):
-    return render(request, 'restaurant/home.html',{})
+    return redirect(restaurant_order) #when logged in redirect to order page
+
+@login_required(login_url = '/restaurant/sign-in/')  #if not logged in redirect to sign-in page
+def restaurant_account(request):
+    user_form = UserFormForEdit(instance =request.user)
+    restaurant_form = RestaurantForm(instance = request.user.restaurant)
+
+
+    if request.method =="POST" : #used to write the changes about the user's information in the database
+        user_form = UserFormForEdit(request.POST, instance = request.user)
+        restaurant_form = RestaurantForm(request.POST,request.FILES, instance = request.user.restaurant) #request.FILES is for the image
+
+    if user_form.is_valid() and restaurant_form.is_valid():
+        user_form.save()
+        restaurant_form.save()
+
+
+
+    return render(request, 'restaurant/account.html',{
+        "user_form": user_form,
+        "restaurant_form": restaurant_form
+    })
+
+@login_required(login_url = '/restaurant/sign-in/')  #if not logged in redirect to sign-in page
+def restaurant_meal(request):
+    meals = Meal.objects.filter(restaurant = request.user.restaurant).order_by("-id")
+    return render(request, 'restaurant/meal.html',{"meals":meals})
+
+@login_required(login_url = '/restaurant/sign-in/')  #if not logged in redirect to sign-in page
+def restaurant_add_meal(request):
+    form = MealForm()
+
+    if request.method == "POST":
+        form = MealForm(request.POST , request.FILES)
+
+        if form.is_valid():
+            meal = form.save(commit=False)
+            meal.restaurant = request.user.restaurant
+            meal.save()
+            return redirect(restaurant_meal)
+
+    return render(request, 'restaurant/edit_meal.html',{
+    "form" : form
+    })
+
+
+@login_required(login_url = '/restaurant/sign-in/')  #if not logged in redirect to sign-in page
+def restaurant_edit_meal(request, meal_id):
+    form = MealForm(instance = Meal.objects.get(id = meal_id))
+
+    if request.method == "POST":
+        form = MealForm(request.POST , request.FILES, instance = Meal.objects.get(id = meal_id))
+
+        if form.is_valid():
+            form.save()
+            return redirect(restaurant_meal)
+
+    return render(request, 'restaurant/edit_meal.html',{
+    "form" : form
+    })
+
+
+
+@login_required(login_url = '/restaurant/sign-in/')  #if not logged in redirect to sign-in page
+def restaurant_order(request):
+
+    if request.method == "POST":
+        order = Order.objects.get(id = request.POST["id"], restaurant = request.user.restaurant)
+        if order.status == Order.COOKING:
+            order.status = Order.READY
+            order.save()
+
+    orders = Order.objects.filter(restaurant = request.user.restaurant).order_by("-id")
+    return render(request, 'restaurant/order.html',{"orders": orders})
+
+@login_required(login_url = '/restaurant/sign-in/')  #if not logged in redirect to sign-in page
+def restaurant_report(request):
+    return render(request, 'restaurant/report.html',{})
 
 
 def restaurant_sign_up(request):             #restaurant registration function
