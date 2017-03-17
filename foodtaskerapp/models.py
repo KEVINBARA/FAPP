9641d2c5647d3833a56c6443c6082adbe729f689
Kevin <baranyizigiyekevin@gmail.com>
2017-03-17 19:46:26 +0300

    test

--- foodtaskerapp/models.py
+++ foodtaskerapp/models.py
@@ -20,7 +20,7 @@ class Customer(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE , related_name = 'customer')
     avatar = models.CharField(max_length= 500)
     phone = models.CharField(max_length= 500)
-    adress = models.CharField(max_length= 500)
+    address = models.CharField(max_length= 500)
 
     def __str__(self):
         return self.user.get_full_name()      #django function used to pull the fullname
@@ -30,7 +30,7 @@ class Driver(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE , related_name = 'driver')
     avatar = models.CharField(max_length= 500)
     phone = models.CharField(max_length= 500)
-    adress = models.CharField(max_length= 500)
+    address = models.CharField(max_length= 500)
 
     def __str__(self):
         return self.user.get_full_name()
@@ -67,7 +67,7 @@ class Order(models.Model):
     address = models.CharField(max_length= 500)
     total = models.IntegerField()
     status = models.IntegerField(choices = STATUS_CHOICES)
-    created_at = models.DateTimeField(default =timezone.now)
+    created_at = models.DateTimeField(default = timezone.now)
     picked_at = models.DateTimeField(blank = True, null = True)
 
     def __str__(self):
