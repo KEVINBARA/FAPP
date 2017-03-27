37e1d946002c6ae03d9ecee9f5c798bbc23fb0e5
Kevin <baranyizigiyekevin@gmail.com>
2017-03-27 14:29:14 +0300

    Added driver's location function

--- foodtaskerapp/apis.py
+++ foodtaskerapp/apis.py
@@ -7,7 +7,7 @@ from django.views.decorators.csrf import csrf_exempt
 from oauth2_provider.models import AccessToken
 
 
-from foodtaskerapp.models import Restaurant, Meal, Order, OrderDetails
+from foodtaskerapp.models import Restaurant, Meal, Order, OrderDetails,Driver
 from foodtaskerapp.serializers import RestaurantSerializer, MealSerializer, OrderSerializer
 
 ###############
@@ -98,6 +98,19 @@ def customer_get_latest_order(request):
     customer = access_token.user.customer
     order = OrderSerializer(Order.objects.filter(customer = customer).last()).data
     return JsonResponse({"order": order})
+
+def customer_driver_location(request):
+    access_token = AccessToken.objects.get(token = request.GET.get("access_token"),
+        expires__gt = timezone.now())
+
+    customer = access_token.user.customer
+
+    # Get driver's location related to this customer's current order
+    current_order = Order.objects.filter(customer = customer, status = Order.ONTHEWAY).last()
+    location = current_order.driver.location
+
+
+    return JsonResponse({"location":location})
 
 ###############
 # RESTAURANT
@@ -215,3 +228,19 @@ def driver_get_revenue(request):
         revenue[day.strftime("%a")] = sum(order.total for order in orders)
 
     return JsonResponse({"revenue": revenue})
+
+
+# POST params : access_token,"latitude,longitutde"
+@csrf_exempt
+def driver_update_location(request):
+    if request.method == "POST":
+        access_token = AccessToken.objects.get(token = request.POST.get("access_token"),
+        expires__gt = timezone.now())
+
+        driver = access_token.user.driver
+
+        # Set location string => database
+        driver.location = request.POST["location"]
+        driver.save()
+
+        return JsonResponse({"status": "success"})
--- foodtaskerapp/models.py
+++ foodtaskerapp/models.py
@@ -19,8 +19,8 @@ class Restaurant(models.Model):
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE , related_name = 'customer')
     avatar = models.CharField(max_length= 500)
-    phone = models.CharField(max_length= 500)
-    address = models.CharField(max_length= 500)
+    phone = models.CharField(max_length= 500, blank = True)
+    address = models.CharField(max_length= 500, blank = True)
 
     def __str__(self):
         return self.user.get_full_name()      #django function used to pull the fullname
@@ -29,8 +29,9 @@ class Customer(models.Model):
 class Driver(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE , related_name = 'driver')
     avatar = models.CharField(max_length= 500)
-    phone = models.CharField(max_length= 500)
-    address = models.CharField(max_length= 500)
+    phone = models.CharField(max_length= 500,blank = True)
+    address = models.CharField(max_length= 500, blank =True)
+    location = models.CharField(max_length = 500 , blank = True)
 
     def __str__(self):
         return self.user.get_full_name()
